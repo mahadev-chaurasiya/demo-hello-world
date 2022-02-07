221f9a67b21db0dfe740365121fc23baa0180b3d
Dev <Dev@sdev>
2022-02-07 19:27:05 +0530

    RequestMapping changes

--- demo-app/src/main/java/com/md/demoapp/HelloWorldController.java
+++ demo-app/src/main/java/com/md/demoapp/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.md.demoapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloWorldController {
  @GetMapping("/hello-world")
  public String helloWorld() {
 	 return "Hello World";
+ }
+ 
+ @RequestMapping(method= RequestMethod.GET, value = "/hello")
+ public String helloRequestMapping() {
+	 return "Hello request mappping";
  }
 }
